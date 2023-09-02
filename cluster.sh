0f07b4477cfa8cee1b53957d9834f035a4af8a98
Mohammad Hassan <62506881+mohammadhy@users.noreply.github.com>
2023-09-02 18:49:37 +0330

    Update cluster.sh
    Change Config

--- cluster.sh
+++ cluster.sh
@@ -1,9 +1,9 @@
-echo "Hello From Github" > /usr/share/message
 #! /bin/bash
+echo "Hello From Github" > /usr/share/message
 server_id=`echo $HOSTNAME | cut -d- -f2`
 #echo $server_id
 
-if [[ server_id -eq 1 ]]; then
+if [[ $server_id -eq 0 ]]; then
     mysql -uroot -p123 -e "create user 'replica'@'%' identified with mysql_native_password by '123';"
     mysql -uroot -p123 -e "grant replication slave on *.* to 'replica'@'%';"
     mysql -uroot -p123 -e "flush privileges;"
