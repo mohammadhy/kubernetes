0e27b2de7812d3f9164b987ee4e8ed1ef1dd92da
Mohammad Hassan <62506881+mohammadhy@users.noreply.github.com>
2023-09-02 18:37:18 +0330

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -4,9 +4,11 @@ server_id=`echo $HOSTNAME | cut -d- -f2`
 #echo $server_id
 
 if [[ server_id -eq 1 ]]; then
-    mysql -uroot -proot -e "create user 'replica'@'%' identified with mysql_native_password by 'passwprd';"
-    mysql -uroot -proot -e "grant replication slave on *.* to 'replica'@'%';"
-    mysql -uroot -proot -e "flush privileges;"
+    mysql -uroot -ppassword -e "create user 'replica'@'%' identified with mysql_native_password by 'passwprd';"
+    mysql -uroot -ppassword -e "grant replication slave on *.* to 'replica'@'%';"
+    mysql -uroot -ppassword -e "flush privileges;"
+    echo "Done Configuration Master"
 else
-   mysql -uroot -proot -e " change replication source to source_host='mysql-0.mysql', source_user='replica_user', source_password='password';"
+   mysql -uroot -ppassword -e " change replication source to source_host='mysql-0.mysql', source_user='replica_user', source_password='password';"
+   echo "Done Configuration Slave"
 fi
